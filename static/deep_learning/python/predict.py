39be6ea105e015c3dd0687299b17f039407e0f71
Wenjun Zhang <1378555845@qq.com>
2024-04-10 01:10:41 -0400

    fix bug

--- static/deep_learning/python/predict.py
+++ static/deep_learning/python/predict.py
@@ -4,20 +4,13 @@ from sklearn.metrics import accuracy_score
 from pathlib import Path
 import os
 import sys
-
-
 from static.deep_learning.python.classification.bert_fc.bert_fc_predictor import BertFCPredictor
 
 def predict(sentence="俄乌战争愈演愈烈，安理会上美国再次使用一票否决权，停战似乎遥遥无期！"):
     title=[]
     line = sentence.strip()
-    
-    line = line.split(' ')
-
     title.append(list(line))
     print(title)
-
-    
     current_dir = Path(__file__).parent
     pretrained_model_dir = current_dir / 'model' / 'bert-base-chinese'
     model_dir = current_dir / 'temp' / 'bertfc'
